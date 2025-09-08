d79ab8395363388f702273d1805064b7ea65ec2e
SPAN <span@SPANs-MacBook-Pro.local>
2025-09-08 14:07:25 +0530

    Initial commit

---
+++ src/app/career.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Career = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Career Guide Screen</Text>
+    </View>
+  );
+};
+
+export default Career;
---
+++ src/app/girlsfirst.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const GirlsFirst = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Girls First Screen</Text>
+    </View>
+  );
+};
+
+export default GirlsFirst;
